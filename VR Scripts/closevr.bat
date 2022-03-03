d445db6922754353165944fdfd46dd895e9e9dec
Antonio Schneider <antoniosch@outlook.com>
2022-03-03 09:28:50 -0300

    Add files via upload

--- VR Scripts/closevr.bat
+++ VR Scripts/closevr.bat
@@ -1,14 +1,15 @@
 @echo off
 
+
+
+"%~dp0\nircmd.exe" nircmd.exe killprocess OVRServer_x64.exe
 "%~dp0\nircmd.exe" closeprocess vrmonitor.exe
 
-TIMEOUT /T 1
-
-"%~dp0\nircmd.exe" killprocess OVRServer_x64.exe
+TIMEOUT /T 2
 
 "C:\Program Files (x86)\Steam\steam.exe" -shutdown
 
-TIMEOUT /T 1
+TIMEOUT /T 2
 
 "%~dp0\nircmd.exe" closeprocess VirtualMotionCapture.exe
 "%~dp0\nircmd.exe" closeprocess VirtualMotionCaptureControlPanel.exe
@@ -21,12 +22,10 @@ TIMEOUT /T 1
 "%~dp0\nircmd.exe" setsysvolume 9830 "Alto-falantes"
 "%~dp0\nircmd.exe" setsysvolume 65535 "BM900"
 
-TIMEOUT /T 20
+"C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -profile1
+
+
 
 "%~dp0\nircmd.exe" closeprocess OculusClient.exe
-
-"%~dp0\OculusDebugToolCLI.exe" -f odt_regular.txt
-
-"C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -profile1
 
 EXIT 0
